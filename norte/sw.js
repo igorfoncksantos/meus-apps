7f5877fe3112acfab4c4b320930df1a4e4445c97
igorfoncksantos <igorfoncksantos@users.noreply.github.com>
2026-08-18 13:27:49 -0300

    Norte: cache-bust do icone (nomes -v2) + SW norte-v2 para forcar atualizacao do icone

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v1';
+const CACHE = 'norte-v2';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
