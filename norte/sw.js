fc7fbdb71149c87ebd137fed29c3b53ab99d3edd
igorfoncksantos <leonardo@uniquebox.com.br>
2026-08-19 09:01:02 -0300

    Norte: recorrência (Diário/Semanal/Mensal/Anual) com dias da semana e calendário

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v3';
+const CACHE = 'norte-v4';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
