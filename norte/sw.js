c767c17b9809a9e7f7eb0d6909b01ce238932ea3
Igor <leonardo@uniquebox.com.br>
2026-08-22 11:12:29 -0300

    Norte: icones SVG no resto (acoes da tarefa, login/PIN/modais, progresso, welcome)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v14';
+const CACHE = 'norte-v15';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
