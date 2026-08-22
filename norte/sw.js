245fd0c639b7415293008fa0c2e0e31211c75644
Igor <leonardo@uniquebox.com.br>
2026-08-22 11:27:27 -0300

    Norte: categorias minimalistas + prioridade URGENTE (cores) + Progresso 2 guias (%/qtd)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v15';
+const CACHE = 'norte-v16';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
