647cef01eaf439470d39e97e3a96dadbc6a61ceb
Igor <leonardo@uniquebox.com.br>
2026-08-22 10:29:23 -0300

    Norte: Apagar vermelho na aba da tarefa + nao pede senha pra abrir (login = sync)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v9';
+const CACHE = 'norte-v10';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
