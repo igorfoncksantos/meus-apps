48638d78db55b2065c6de3cf5ec7d1ff40b66e19
Igor <leonardo@uniquebox.com.br>
2026-08-22 10:56:08 -0300

    Norte: PIN opcional de 4 digitos (teclado proprio, trava local por aparelho)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v12';
+const CACHE = 'norte-v13';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
