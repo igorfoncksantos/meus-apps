7a93a19a0ee4d6473ba3464122d94b4e655228df
Igor <leonardo@uniquebox.com.br>
2026-08-22 11:35:44 -0300

    Norte: Progresso mostra as 2 metricas juntas (percentual + n de tarefas)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v17';
+const CACHE = 'norte-v18';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
