438c0d7523b44ec420da94815fb657da7390c9e8
Igor <leonardo@uniquebox.com.br>
2026-08-22 10:19:03 -0300

    Norte: Zerar tudo vermelho + tirar senha exige a senha atual

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v8';
+const CACHE = 'norte-v9';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
