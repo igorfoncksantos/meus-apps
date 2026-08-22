e10cfc47eafdb2dac647138590c076f8de5717e1
igorfoncksantos <leonardo@uniquebox.com.br>
2026-08-22 12:50:04 -0300

    Stash: aba Cashbook com layout de aneis (hero+aneis SVG+editores). Cashbook: picker de 50 icones SVG na categoria + icones da moldura em SVG. Norte: nichos com 3D mais forte. icons.js +35 icones de categoria. SW bump

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v19';
+const CACHE = 'norte-v20';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
