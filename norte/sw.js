291f564b28d06b5f9e08fd68844569c39423c418
Igor <leonardo@uniquebox.com.br>
2026-08-25 09:01:01 -0300

    Norte v30: barras nao invadem os eixos (primeira/ultima coluna)

--- norte/sw.js
+++ norte/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'norte-v29';
+const CACHE = 'norte-v30';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil((async () => {
   const ks = await caches.keys();
